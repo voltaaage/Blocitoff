acd005203f8eb62b8c503e901486ed5b83c8dafb
Fong <fong.fan.90@gmail.com>
2015-09-22 23:26:28 -0500

    fixing faker line

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-require 'Faker'
+require 'faker'
 
 admin = User.new(
   name: 'Admin User',
